297ae297a8ea91dce8a54c2c684f30dc22654566
Jack Tolhurst <112444794+jacktolhurst@users.noreply.github.com>
2026-09-22 16:24:28 +1000

    Album Shows up

--- .database/setup.sql
+++ .database/setup.sql
@@ -1,5 +1,7 @@
-CREATE TABLE FansOnly(id AutoNumber NOT NULL PRIMARY KEY, name TEXT NOT NULL, type BOOL NOT NULL, smallInfo TEXT NOT NULL, description TEXT NOT NULL, image TEXT NOT NULL);
+SELECT AVG(type) FROM FansOnly;
 
-INSERT INTO FansOnly(id, name, type, smallInfo, description, image) VALUES (1, "TestFan", True, "Yap Yap", "YAP YAP YAP", "Link");
+-- CREATE TABLE FansOnly(id AutoNumber NOT NULL PRIMARY KEY, name TEXT NOT NULL, type BOOL NOT NULL, smallInfo TEXT NOT NULL, description TEXT NOT NULL, image TEXT NOT NULL);
 
-DROP TABLE FansOnly;
+-- INSERT INTO FansOnly(id, name, type, smallInfo, description, image) VALUES (50, "TestFan", True, "Yap Yap", "YAP YAP YAP", "Link");
+
+-- DROP TABLE FansOnly;
